114a01f8e23b6e394a960dc41679661ef75a780d
sid <prashanth.s037>
2018-06-24 21:58:04 +0200

    Corrected multi class Salum1 segmentation generator

--- data_preprocessors/Salum1SegGenerator.py
+++ data_preprocessors/Salum1SegGenerator.py
@@ -2,6 +2,8 @@ import os
 import cv2 as cv
 import numpy as np
 
+defect_classes = [1, 3, 5, 6, 7, 8, 12, 15, 17, 20, 22, 24, 26, 27, 28, 29, 30, 31, 38, 39, 41]
+
 def generateTwoClassSegMaps(path_to_imgs, 
   path_to_def_files, 
   save_path, 
@@ -72,7 +74,8 @@ def generateMultiClassSegMaps(path_to_imgs,
         os.path.join(path_to_def_files, img[:-4] + ".def"),
         skip_header=7, delimiter=' ', usecols=(0,1,2,3,4), dtype='int')
     
-    mask = np.zeros((im.shape[0], im.shape[1], 21), dtype='uint8')
+    #two dimentional mask is enough bcos data generators convert them to one hot vec
+    mask = np.zeros((im.shape[0], im.shape[1]), dtype='uint8')
     
     for defect in defects:
 
@@ -81,6 +84,7 @@ def generateMultiClassSegMaps(path_to_imgs,
       #mask for two class segmentation. 
       
       if def_type not in ignore_defects:
-        mask[y1:y2, x1:x2] = 1
+        idx = defect_classes.index(def_type)
+        mask[y1:y2, x1:x2] = idx
       
   cv.imwrite(os.path.join(save_path, img[:-4] + '.png'), mask)
