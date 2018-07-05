71af5a8cb180e9f2044447f548e991dd6da8e92e
psid <prashanth.siddagangaiah@st.ovgu.de>
2018-07-05 12:22:23 +0300

    Modified loss function to accept class weights

--- loss.py
+++ loss.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 
-def pixelwise_crossentropy(target, output):
+def weighted_pixelwise_crossentropy(cls_weights=[1]):
+  def pixelwise_crossentropy(target, output):
+    
+    output = tf.clip_by_value(output, 10e-8, 1. - 10e-8)
+    return - tf.reduce_sum(target * cls_weights * tf.log(output))
   
-  weights = [0.05, 0.95]
-  output = tf.clip_by_value(output, 10e-8, 1. - 10e-8)
-  return - tf.reduce_sum(target * weights * tf.log(output))
+  return pixelwise_crossentropy
