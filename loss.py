5387d60006d048fad435d49d51084bd51f90a196
psid <prashanth.siddagangaiah@st.ovgu.de>
2018-07-09 14:55:26 +0300

    Cosmetic changes

--- loss.py
+++ loss.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 
 def weighted_pixelwise_crossentropy(cls_weights=[1]):
-  def pixelwise_crossentropy(target, output):
-    
-    output = tf.clip_by_value(output, 10e-8, 1. - 10e-8)
-    return - tf.reduce_sum(target * cls_weights * tf.log(output))
-  
-  return pixelwise_crossentropy
+
+    def pixelwise_crossentropy(target, output):
+
+        output = tf.clip_by_value(output, 10e-8, 1. - 10e-8)
+        return - tf.reduce_sum(target * cls_weights * tf.log(output))
+
+    return pixelwise_crossentropy
