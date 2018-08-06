b1f2bfbf3c8446b15a4af6ba142a878eaeb2974d
sid <prashanth.s037>
2018-08-06 04:32:57 +0200

    Added function to predict on GAP

--- util/model_eval_and_predict_on_GAP.py
+++ util/model_eval_and_predict_on_GAP.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 from data_generators.batch_generators import gen_for_GAP_data
 
 def evaluate_model_on_GAP(model, split_name, n_channels):
@@ -16,3 +17,29 @@ def evaluate_model_on_GAP(model, split_name, n_channels):
   print(result)
   
   return result
+
+def predict_model_on_GAP(model, split_name, n_channels):
+  gen = None
+  
+  predictions = []
+  
+  labels = []
+  
+  gen = None
+  
+  gen, steps = gen_for_GAP_data(split_name, n_channels)
+  
+  if gen is None: 
+    print("split_name didnt match test or valid. Exiting!!!")
+    sys.exit() 
+
+  for i in range(steps):
+    X,Y = next(gen)
+    result = model.predict_on_batch(X)
+    
+    labels= np.append(labels,Y[:,1])
+
+    predictions = np.append(predictions, result[:,1])
+
+   
+  return predictions, labels
